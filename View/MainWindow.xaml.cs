63dc4e1911bd8fecf6cb6eb0b55991e9c2aedfc7
realShirai <sebisch2727@hotmail.com>
2022-11-27 14:33:21 +0100

    Add MainViewModel
    Add ViewModel for MainWindow

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LevelEditor.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace LevelEditor
     {
         public MainWindow()
         {
+            DataContext = new MainViewModel();
             InitializeComponent();
         }
     }
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using System.Windows.Media.Animation;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Printing;
+using CommunityToolkit.Mvvm.Input;
+using System.IO;
+
+namespace LevelEditor.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
